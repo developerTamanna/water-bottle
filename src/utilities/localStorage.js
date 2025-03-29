830c0d509ac964352581e16552fd2f91919a2eb3
developerTamanna <astamanna57@gmail.com>
2025-03-29 15:54:44 +0600

    localStorage

---
+++ src/utilities/localStorage.js
@@ -0,0 +1,31 @@
+// ***  1. to getSomething from local storage you will get it as a string
+
+//**2
+//*  convert this to javaScript object
+//  */
+
+const getCartFormLocalStorage =()=>{
+    const storedCartString = localStorage.getItem('cart');
+    if(storedCartString){
+        const storedCart = JSON.parse(storedCartString);
+        return storedCart;
+    }
+    return[];
+}
+
+
+const saveCartToLocalStorage = cart =>{
+    const cartStringified = JSON.stringify(cart);
+    localStorage.setItem('cart', cartStringified);
+}
+
+
+const addItemToCartLocalStorage = id=>{
+    const cart = getCartFormLocalStorage()
+    // cart.push(id)
+    const newCart = [...cart, id]
+    //**save new cart to the local storage */
+    saveCartToLocalStorage(newCart)
+}
+
+export {getCartFormLocalStorage, addItemToCartLocalStorage}
